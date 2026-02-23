ddfe88ac79a21411fa75fa7fef509404c26b35b5
Om-Prakash-Samal <omsamal6002@gmail.com>
2026-02-23 15:04:31 +0530

    Added UsecaseName2”

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,8 +1,9 @@
-// version 1.0
+// version 2.0
 //author Abc
-//useCase 1: Welcome page
+//useCase 2:
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("Modify this logic to usecase 2");
     }
 }
